755b613787cd695f4ab8293a4040065d95b683e6
Ray Luxembourg <rayluxembourg@gmail.com>
2022-01-18 00:57:57 +0200

    handle capital letters

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   }, [])
 
   const onKeyDown = (e: KeyboardEvent) => {
-    setPressedKey(e.key)
+    setPressedKey(e.key.toLowerCase())
     setTimeout(() => {
       setPressedKey('')
     },100)
